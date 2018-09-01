75020cd435672c8eec5ad8f09b4ff3ab3500cd1f
Nasser Sanou <nassersanou23@gmail.com>
2018-09-01 07:15:04 +0000

    Bachelor: Completed

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 ##Get Winners
 def get_first_name_of_season_winner(data, season)
     data[season].each do |actor| 
-    if actor["status"] == "Winner"
+    unless actor["status"] != "Winner"
     fn = actor["name"].split(" ")
     #or.split().first ||split()[0]
     fn.each do |first, last| #splited to first & last
@@ -16,7 +16,7 @@ end
 def get_contestant_name(data, occupation)
     data.each do | season, details|
     details.each do |actor|
-    if actor["occupation"] == occupation
+    unless actor["occupation"] != occupation #negating if
     return actor["name"]  
     end 
   end 
@@ -37,11 +37,26 @@ def count_contestants_by_hometown(data, hometown)
 return numb_of_actors_by_homewtown
 end
 
-#Get Contestants Occupations
+#Get Contestants Occupations when given H-town 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season, details|
+      details.each do |actor|
+        unless actor["hometown"] != hometown
+          return actor["occupation"] 
+        end 
+      end 
+    end 
 end
 
+#Get Avrage Age
 def get_average_age_for_season(data, season)
-  # code here
-end
+
+    numb_of_actors = 0
+    ageCounter = 0
+    data[season].each do |actor|
+    numb_of_actors += 1 #count actors 
+    ageCounter += actor["age"].to_f #add actors ages 
+    end 
+      return (ageCounter / numb_of_actors).round # get average with division 
+ end
+
