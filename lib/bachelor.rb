b4799f26a463679c174fee6c90f99573facf97a3
Nasser Sanou <nassersanou23@gmail.com>
2018-09-01 06:24:27 +0000

    in progress..

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,27 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data[season].each do |actor| 
+    if actor["status"] == "Winner"
+    fn = actor["name"].split(" ")
+    #or.split().first ||split()[0]
+    fn.each do |first, last| #splited to first & last
+      return first 
+    end 
+    end 
+  end
 end
 
+
+
 def get_contestant_name(data, occupation)
-  # code here
-end
+    data.each do | season, details|
+    details.each do |actor|
+    if actor["occupation"] == occupation
+    return actor["name"]  
+    end 
+  end 
+ end
+end 
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
